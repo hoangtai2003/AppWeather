cb3a3b25b0b6e99d4ec16fc53b835cbe31b295cb
taiht <Hdt13102k3@gmail.com>
2024-05-25 00:55:05 +0700

    truy cap vi tri nguoi dung

--- app/src/main/java/com/example/weather/view/SplashActivity.java
+++ app/src/main/java/com/example/weather/view/SplashActivity.java
@@ -1,60 +1,145 @@
 package com.example.weather.view;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
-import android.os.Handler;
 import android.widget.Toast;
 
 import com.example.weather.R;
 import com.example.weather.model.WeatherApp;
 import com.example.weather.network.ApiInterface;
-import com.example.weather.network.CheckInternet;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class SplashActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int LOCATION_REQUEST_CODE = 10001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        if (CheckInternet.isNetworkAvailable(this))
-            fetWeatherData("Hanoi");
-        else
-            Toast.makeText(SplashActivity.this, "Không có kết nối mạng, vui lòng thử lại!", Toast.LENGTH_SHORT).show();
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        requestLocationPermission();
     }
 
-    private void fetWeatherData(String cityName) {
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            getLastKnownLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastKnownLocation();
+            } else {
+                showExplanationDialog();
+            }
+        }
+    }
+
+    private void getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location != null) {
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+                        getCityNameFromLocation(latitude, longitude);
+                    } else {
+                        finish();
+                    }
+                });
+    }
+
+    private void getCityNameFromLocation(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                String cityName = addresses.get(0).getLocality();
+                if (cityName != null) {
+                    fetchWeatherData(cityName);
+                } else {
+                    Toast.makeText(SplashActivity.this, "Không thể xác minh tên thành phố, vui lòng thử lại", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(SplashActivity.this, "Không thể lấy dữ liệu thời tiết, vui lòng thử lại", Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    private void fetchWeatherData(String cityName) {
         ApiInterface.apiInterface.getweatherData(cityName, "9d25492b3c5d467f46369b1d01a67d7a", "metric").enqueue(new Callback<WeatherApp>() {
             @Override
             public void onResponse(@NonNull Call<WeatherApp> call, @NonNull Response<WeatherApp> response) {
                 WeatherApp responseBody = response.body();
                 if (response.isSuccessful() && responseBody != null) {
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                            intent.putExtra("weatherData", responseBody);
-                            startActivity(intent);
-                            finish();
-                        }
-                    }, 3000); // Thời gian delay 3000ms (3 giây)
+                    navigateToMainActivity(responseBody);
+                } else {
+                    Toast.makeText(SplashActivity.this, "Không thể lấy dữ liệu thời tiết, vui lòng thử lại", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<WeatherApp> call, @NonNull Throwable t) {
-
+                Toast.makeText(SplashActivity.this, "Không có kết nối mạng, vui lòng thử lại", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void navigateToMainActivity(WeatherApp weatherApp) {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        intent.putExtra("weatherData", weatherApp);
+        startActivity(intent);
+        finish();
+    }
+    private void showExplanationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quyền Truy Cập Vị Trí");
+        builder.setMessage("Ứng dụng cần quyền truy cập vị trí để hiển thị dữ liệu thời tiết. Vui lòng cấp quyền truy cập để tiếp tục.");
+        builder.setPositiveButton("Đồng Ý", (dialogInterface, i) -> ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE));
+        builder.setNegativeButton("Từ Chối", (dialogInterface, i) -> {
+            finish();
+        });
+        builder.setCancelable(false);
+        builder.show();
     }
 }
